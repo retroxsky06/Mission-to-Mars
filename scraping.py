626ee1abb916dfbb0844e972b35738b70d29b73f
Vanessa Aczon <aczon.vanessa@gmail.com>
2021-09-02 23:01:41 -0700

    Uploading Challenge Deliverables 1, 2, 3

--- scraping.py
+++ scraping.py
@@ -18,7 +18,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
     
 # Stop webdriver and return data
@@ -95,6 +96,29 @@ def mars_facts():
     df.set_index('Description', inplace=True)
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+# Deliverable 2.Update the Web App with Mars’s Hemisphere Images and Titles
+def hemispheres(browser):
+# Visit url site
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+# Retrieve hemisphere images and titles
+    hemisphere_image_urls = []
+    for i in range(4):
+        
+        hemispheres = {}
+        
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    
+    return hemisphere_image_urls
 
 # Stop webdriver and return data
 #    browser.quit()
